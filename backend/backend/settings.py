979c1b03cb218f1e8aa5a9ba442ef83fd571e7df
Rokon Ron <rokon.raz@gmail.com>
2023-08-16 01:27:30 +0600

    install djago rest_frameworks here.and aslo update views and urls file.and some urls pattens here.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'base.apps.BaseConfig',
+    
+    
+    'rest_framework',
+    
+    
+    
 ]
 
 MIDDLEWARE = [
