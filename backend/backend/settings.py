c3790013beb26124d1ea68e111c7656a2784da1e
Rokon Ron <rokon.raz@gmail.com>
2023-08-16 18:29:56 +0600

    intall axios and setup this stage.and also change fontend homescreen and productscreen some functionality.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,12 +42,15 @@ INSTALLED_APPS = [
     
     
     'rest_framework',
+    'corsheaders',
     
     
     
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,9 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True 
